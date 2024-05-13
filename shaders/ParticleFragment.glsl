bd62cb89781fa12251da86c381222840ffccd73b
chewbeccaroni <159254885+chewbeccaroni@users.noreply.github.com>
2024-05-13 18:26:00 +0100

    Update ParticleFragment.glsl

--- shaders/ParticleFragment.glsl
+++ shaders/ParticleFragment.glsl
@@ -22,22 +22,93 @@
 //    else
 //        brightColour = vec4(0.0, 0.0, 0.0, 1.0);
 //}
+//#version 410 core
+//
+//layout(location=0) out vec4 fragColour;
+//in vec2 particleColour;
+//in vec2 TexCoords;
+//uniform sampler2D screenTexture;
+//
+//
+//void main()
+//{
+////      fragColour= particleColour * offset[4];
+////    vec4 sum = vec4(0.0);
+////    float blur = radius/resolution;
+////
+////    float hstep = dir.x;
+////    float vstep = dir.y;
+////
+////    fragColour = vec4(hstep * vstep);
+//
+////    sum += fragColour(vec2())
+//    vec2 offsets[9] = vec2[](
+//    vec2(-1,1),
+//    vec2( 0.0f,    1), // top-center
+//    vec2( 1,  1), // top-right
+//    vec2(-1,  0.0f),   // center-left
+//    vec2( 0.0f,    0.0f),   // center-center
+//    vec2( 1,  0.0f),   // center-right
+//    vec2(-1, -1), // bottom-left
+//    vec2( 0.0f,   -1), // bottom-center
+//    vec2( 1, -1)
+//);
+//
+//    float kernel [9] = float[](
+//     1.0/16 , 2.0/16 , 1.0/16 ,
+//    2.0/16, 4.0/16, 2.0/16,
+//    1.0/16, 2.0/16 , 1.0/16
+//    );
+//
+//    vec3 samTex[9];
+//    for (int i = 0; i < 9; i++)
+//    {
+//        samTex[i] = vec3(texture(screenTexture, TexCoords.st + offsets[i]));
+//    }
+//
+//    vec3 col = vec3(0.0);
+//    for (int i = 0; i < 9; i++)
+//    {
+//        col += samTex[i] * kernel[i];
+//    }
+//
+//    fragColour = vec4(col, 5.0);
+//}
+
 #version 410 core
 
 layout(location=0) out vec4 fragColour;
-in vec4 particleColour;
-in vec3 fs_Normal;
-uniform float offset[5] = float[](0.0, 1.0, 2.0, 3.0, 4.0);
-uniform float weight[5] = float[](0.2270270270, 0.1945945946, 0.1216216216, 0.0540540541, 0.0162162162);
-out vec4 out_Normal;
-out vec4 out_Colour;
+in vec2 particleColour;
+in vec2 TexCoords;
+uniform sampler2D screenTexture;
 
+// Define kernel and offsets as uniform arrays
 void main()
 {
-    fragColour=particleColour * weight[0];
-    for (int i=1; i<5; i++)
-    {
-        fragColour += particleColour * weight[i];
-    }
-}
+const float kernel[9] = float[](
+1.0 / 16, 2.0 / 16, 1.0 / 16,
+2.0 / 16, 4.0 / 16, 2.0 / 16,
+1.0 / 16, 2.0 / 16, 1.0 / 16
+);
 
+const vec2 offsets[9] = vec2[](
+vec2(-1,  1), vec2( 0,  1), vec2( 1,  1),
+vec2(-1,  0), vec2( 0,  0), vec2( 1,  0),
+vec2(-1, -1), vec2( 0, -1), vec2( 1, -1)
+);
+
+
+    vec3 samTex[9];
+    for (int i = 0; i < 9; i++)
+    {
+        samTex[i] = vec3(texture(screenTexture, TexCoords + offsets[i]));
+    }
+
+    vec3 col = vec3(0.0);
+    for (int i = 0; i < 9; i++)
+    {
+        col += samTex[i] * kernel[i];
+    }
+
+    fragColour = vec4(col, 6.0);
+}
