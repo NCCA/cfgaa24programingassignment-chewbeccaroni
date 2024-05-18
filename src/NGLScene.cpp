42028e72f14aa3968c2eb415e373868dc401dc44
chewbeccaroni <159254885+chewbeccaroni@users.noreply.github.com>
2024-05-18 15:58:51 +0100

    Update NGLScene.cpp

--- src/NGLScene.cpp
+++ src/NGLScene.cpp
@@ -31,82 +31,153 @@ void NGLScene::resizeGL(int _w , int _h)
 }
 
 
-void NGLScene::initializeGL() {
-    // we must call that first before any other GL commands to load and link the
-    // gl commands from the lib, if that is not done program will crash
+void NGLScene::initializeGL()
+{
+    // Existing code...
     ngl::NGLInit::initialize();
-    glClearColor(0.7f, 0.6f, 1.0f, 1.0f);               // Grey Background
-    // enable depth testing for drawing
+    glClearColor(0.7f, 0.6f, 1.0f, 1.0f);
     glEnable(GL_DEPTH_TEST);
-    // enable multisampling for smoother drawing
     glEnable(GL_MULTISAMPLE);
-    ngl::ShaderLib::loadShader("ParticleShader","shaders/ParticleVertex.glsl","shaders/ParticleFragment.glsl");
+    ngl::ShaderLib::loadShader("ParticleShader","shaders/ParticleVertex.glsl","shaders/ParticleFragment.glsl","shaders/ParticleBlur.glsl");
     ngl::ShaderLib::use("ParticleShader");
-    m_view=ngl::lookAt({15,15,15},{0,0,0},{0,1,0});
+    m_view = ngl::lookAt({15,15,15},{0,0,0},{0,1,0});
+
+    // Step 1: Set up framebuffer and textures
+    glGenFramebuffers(1, &hdrFBO);
+    glBindFramebuffer(GL_FRAMEBUFFER, hdrFBO);
+
+    glGenTextures(2, colorBuffers);
+    for (unsigned int i = 0; i < 2; i++)
+    {
+        glBindTexture(GL_TEXTURE_2D, colorBuffers[i]);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB16F, m_win.width, m_win.height, 0, GL_RGB, GL_FLOAT, nullptr);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + i, GL_TEXTURE_2D, colorBuffers[i], 0);
+    }
+
+    unsigned int attachments[2] = { GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1 };
+    glDrawBuffers(2, attachments);
+
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        std::cout << "Framebuffer is not complete!" << std::endl;
+
+    glGenFramebuffers(2, blurFBO);
+    glGenTextures(2, blurTextures);
+    for (unsigned int i = 0; i < 2; i++)
+    {
+        glBindFramebuffer(GL_FRAMEBUFFER, blurFBO[i]);
+        glBindTexture(GL_TEXTURE_2D, blurTextures[i]);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB16F, m_win.width, m_win.height, 0, GL_RGB, GL_FLOAT, nullptr);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, blurTextures[i], 0);
+
+        if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+            std::cout << "Blur Framebuffer " << i << " is not complete!" << std::endl;
+    }
+
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
 
+void NGLScene::timerEvent(QTimerEvent *_event)
+{
+    update();
+}
 
 
 void NGLScene::paintGL()
 {
-  // clear the screen and depth buffer
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-  glViewport(0,0,m_win.width,m_win.height);
-  ngl::Transformation tx;
+    // First pass: render the scene to framebuffer
+    glBindFramebuffer(GL_FRAMEBUFFER, hdrFBO);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glViewport(0, 0, m_win.width, m_win.height);
 
-  tx.setScale(1.0f,1.0f,1.0f);
-  tx.setPosition(0.0f,0.0f,0.0f);
-  ngl::ShaderLib::setUniform("Colour", 1.0f, 0.0f, 0.0f, 1.0f);
-  ngl::ShaderLib::setUniform("MVP", m_project*m_view*tx.getMatrix());
-  ngl::VAOPrimitives::draw(ngl::cube);
-
-  tx.reset();
-//  tx.setPosition(1.3f,0.5f,0.0f);
-//  tx.getInverseMatrix();
-//  tx.setScale(0.5f,0.5f,0.5f);
-//  tx.addRotation(0,0,-45);
-//  ngl::ShaderLib::setUniform("Colour", 0.9f, 0.67f, 0.4f, 1.0f);
-//  ngl::ShaderLib::setUniform("MVP", m_project*m_view*tx.getMatrix());
-//  ngl::VAOPrimitives::draw(ngl::teapot);
+    // Render your scene here
     auto rotX = ngl::Mat4::rotateX(m_win.spinXFace);
     auto rotY = ngl::Mat4::rotateY(m_win.spinYFace);
     auto mouseRotation = rotX * rotY;
     mouseRotation.m_m[3][0] = m_modelPos.m_x;
     mouseRotation.m_m[3][0] = m_modelPos.m_x;
     mouseRotation.m_m[3][0] = m_modelPos.m_x;
+    ngl::Transformation tx;
 
-//    ngl::ShaderLib::setUniform("MVP",m_project*m_view*mouseRotation);
-    glPointSize(4);
+    tx.setScale(1.0f, 1.0f, 1.0f);
+    tx.setPosition(0.0f, 0.0f, 0.0f);
+    ngl::ShaderLib::setUniform("Colour", 1.0f, 0.0f, 0.0f, 1.0f);
+    ngl::ShaderLib::setUniform("MVP", m_project * m_view * mouseRotation);
+    ngl::ShaderLib::use("ParticleShader");
+    ngl::VAOPrimitives::draw("dragon");
 
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    // Step 4: Apply horizontal and vertical blur
+    bool horizontal = true, first_iteration = true;
+    unsigned int amount = 10;
     ngl::ShaderLib::use("ParticleShader");
 
+    for (unsigned int i = 0; i < amount; i++)
+    {
+        glBindFramebuffer(GL_FRAMEBUFFER, blurFBO[horizontal]);
+        glUniform1i(glGetUniformLocation(ngl::ShaderLib::getShaderID("ParticleShader"), "horizontal"), horizontal);
+        glBindTexture(GL_TEXTURE_2D, first_iteration ? colorBuffers[1] : blurTextures[!horizontal]);
+        renderQuad();
+        horizontal = !horizontal;
+        if (first_iteration)
+            first_iteration = false;
+    }
 
-// set up floating point framebuffer to render scene to
-//    unsigned int hdrFBO;
-//    glGenFramebuffers(1, &hdrFBO);
-//    unsigned int attachments[2] = { GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1 };
-//    glDrawBuffers(2, attachments);
-//    glBindFramebuffer(GL_FRAMEBUFFER, hdrFBO);
-//    unsigned int colourBuffers[2];
-//    glGenTextures(2, colourBuffers);
-//    for (unsigned int i = 0; i < 2; i++)
-//    {
-//        glBindTexture(GL_TEXTURE_2D, colourBuffers[i]);
-//        glTexImage2D(
-//                GL_TEXTURE_2D, 0, GL_RGBA16F, SCHAR_WIDTH, 8, 0, GL_RGBA, GL_FLOAT, NULL
-//        );
-//        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-//        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-//        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-//        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-//        // attach texture to framebuffer
-//        glFramebufferTexture2D(
-//                GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + i, GL_TEXTURE_2D, colourBuffers[i], 0
-//        );
-//    }
-
-
+    // Now render quad with blurred texture to default framebuffer
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glBindTexture(GL_TEXTURE_2D, blurTextures[!horizontal]);
+    renderQuad();
 }
+
+void NGLScene::renderQuad()
+{
+    static unsigned int quadVAO = 0;
+    static unsigned int quadVBO;
+    if (quadVAO == 0)
+    {
+        float quadVertices[] = {
+                // positions        // texture Coords
+                -1.0f,  1.0f, 0.0f,  0.0f, 1.0f,
+                -1.0f, -1.0f, 0.0f,  0.0f, 0.0f,
+                1.0f, -1.0f, 0.0f,  1.0f, 0.0f,
+
+                -1.0f,  1.0f, 0.0f,  0.0f, 1.0f,
+                1.0f, -1.0f, 0.0f,  1.0f, 0.0f,
+                1.0f,  1.0f, 0.0f,  1.0f, 1.0f
+        };
+        glGenVertexArrays(1, &quadVAO);
+        glGenBuffers(1, &quadVBO);
+        glBindVertexArray(quadVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    }
+    glBindVertexArray(quadVAO);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    glBindVertexArray(0);
+}
+
+
+
+
+
+//    ngl::ShaderLib::setUniform("MVP",m_project*m_view*mouseRotation);
+
+
+
+
 
 //----------------------------------------------------------------------------------------------------------------------
 
