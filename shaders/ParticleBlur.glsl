fe33029dbcee6433f779655d64a2315f2aebb6c7
chewbeccaroni <159254885+chewbeccaroni@users.noreply.github.com>
2024-05-21 15:13:33 +0100

    Update ParticleBlur.glsl

--- shaders/ParticleBlur.glsl
+++ shaders/ParticleBlur.glsl
@@ -1,5 +1,36 @@
-#version 120
+#version 410 core
+out vec4 fragColour;
 
-void main() {
+in vec2 TexCoords;
 
+uniform sampler2D screenTexture;
+uniform bool horizontal;
+
+const float weight[5] = float[](
+0.227027, 0.1945946, 0.1216216, 0.054054, 0.016216
+);
+
+void main()
+{
+    vec2 tex_offset = 1.0 / textureSize(screenTexture, 0); // gets size of single texel
+    vec3 result = fragColour.rgb * weight[4]; // current fragment weight contribution
+
+    if(horizontal)
+    {
+        for(int i = 1; i < 5; ++i)
+        {
+            result += texture(screenTexture, TexCoords + vec2(tex_offset.x * i, 0.0)).rgb * weight[i];
+            result += texture(screenTexture, TexCoords - vec2(tex_offset.x * i, 0.0)).rgb * weight[i];
+        }
+    }
+    else
+    {
+        for(int i = 1; i < 5; ++i)
+        {
+            result += texture(screenTexture, TexCoords + vec2(0.0, tex_offset.y * i)).rgb * weight[i];
+            result += texture(screenTexture, TexCoords - vec2(0.0, tex_offset.y * i)).rgb * weight[i];
+        }
+    }
+
+    fragColour = vec4(result, 1.0);
 }
